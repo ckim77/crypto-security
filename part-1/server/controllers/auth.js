680ff149a42d11ad8984ba39c43669f3987913fe
Chris Kim <ckjr92@gmail.com>
2021-11-04 13:55:15 -0600

    finished step 1

--- part-1/server/controllers/auth.js
+++ part-1/server/controllers/auth.js
@@ -1,21 +1,43 @@
-const users = []
+const bcrypt = require("bcrypt");
+const users = [];
 
 module.exports = {
+    
     login: (req, res) => {
       console.log('Logging In User')
-      console.log(req.body)
-      const { username, password } = req.body
+      // console.log(req.body)
+      const {username, password} = req.body;
+      // console.log(password);
+      
       for (let i = 0; i < users.length; i++) {
-        if (users[i].username === username && users[i].password === password) {
+        const existingPassword = bcrypt.compareSync(password, users[i].passwordHash);
+        // console.log(existingPassword);
+        
+        if (users[i].username === username && existingPassword === true) {
+          console.log("Success!");
           res.status(200).send(users[i])
+        } else {
+          res.status(400).send("User not found.")
         }
       }
-      res.status(400).send("User not found.")
     },
+    
     register: (req, res) => {
-        console.log('Registering User')
-        console.log(req.body)
-        users.push(req.body)
-        res.status(200).send(req.body)
+
+        const salt = bcrypt.genSaltSync(); // default is 10
+        const passwordHash = bcrypt.hashSync(req.body.password,salt);
+        
+        let userObj = {
+          username: req.body.username,
+          email: req.body.email,
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          passwordHash
+        }
+        
+        users.push(userObj)
+        res.status(200).send(userObj)
+        //console.log('Registering User')
+        console.log(users)
     }
 }
